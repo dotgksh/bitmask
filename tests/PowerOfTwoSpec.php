4288a2dc2beca1c118e49f2afb389324effffd2c
Gustavo Karkow <14905932+karkowg@users.noreply.github.com>
2024-05-03 15:20:43 -0400

    chore: remove PowerOfTwoSpec test repetitions

--- tests/PowerOfTwoSpec.php
+++ tests/PowerOfTwoSpec.php
@@ -37,26 +37,22 @@ it('is not power of two w/ random int from 0 to 255', function (int $value) {
     new PowerOfTwo($value);
 })
     ->with($notPowerOfTwoGenerator(min: 0, max: MaxValue::UInt8->value))
-    ->repeat(5)
     ->throws(InvalidArgumentException::class);
 
 it('is not power of two w/ random int from 256 to 65535', function (int $value) {
     new PowerOfTwo($value);
 })
     ->with($notPowerOfTwoGenerator(min: MaxValue::UInt8->value + 1, max: MaxValue::UInt16->value))
-    ->repeat(5)
     ->throws(InvalidArgumentException::class);
 
 it('is not power of two w/ random int from 65536 to 16777215', function (int $value) {
     new PowerOfTwo($value);
 })
     ->with($notPowerOfTwoGenerator(min: MaxValue::UInt16->value + 1, max: MaxValue::UInt24->value))
-    ->repeat(5)
     ->throws(InvalidArgumentException::class);
 
 it('is not power of two w/ random int from 16777216 to 4294967295', function (int $value) {
     new PowerOfTwo($value);
 })
     ->with($notPowerOfTwoGenerator(min: MaxValue::UInt24->value + 1, max: MaxValue::UInt32->value))
-    ->repeat(5)
     ->throws(InvalidArgumentException::class);
